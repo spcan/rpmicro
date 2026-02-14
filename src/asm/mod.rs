4a581135cc88950b7337296588070c5591f51749
Andres <agrc14@gmail.com>
2026-02-14 16:33:43 +0100

    Add memory ASM instructions

--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -18,3 +18,18 @@ pub fn sev() {
 pub fn bkpt() {
     unsafe { asm!("bkpt", options(nomem, nostack, preserves_flags)) }
 }
+
+#[inline(always)]
+pub fn dsb() {
+    unsafe { asm!("dsb", options(nomem, nostack, preserves_flags)) }
+}
+
+#[inline(always)]
+pub fn dmb() {
+    unsafe { asm!("dmb", options(nomem, nostack, preserves_flags)) }
+}
+
+#[inline(always)]
+pub fn isb() {
+    unsafe { asm!("isb", options(nomem, nostack, preserves_flags)) }
+}
